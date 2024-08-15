2089539a5943f86e605de69c3fd3fa40f2239734
AJi <159735903+jisxc@users.noreply.github.com>
2024-08-15 17:15:48 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/HomePage";
+import AboutPage from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+function App() {
+  return (
+    //font awesome cdn
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+ 
